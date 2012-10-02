2715d655e9a665d2670293757b6d06513761b051
Joseph Chilcote <chilcote+github@gmail.com>
2012-10-02 13:35:32 -0700

    added section for the body of the novel

--- fanowrimo.py
+++ fanowrimo.py
@@ -3,7 +3,7 @@
 """ fanowrimo.py
 	by Joseph Chilcote
 	chilcote@gmail.com
-	last modified: 2012-9-30 """
+	last modified: 2012-10-2 """
 
 from sys import argv
 from sys import exit
@@ -15,24 +15,68 @@ script, novel = argv
 byline = "a novel by Ernst Hummingway"
 toc = "Table of Contents"
 the_end = "THE END"
-remainder = 50000 - len(byline.split(" ")) - len(toc.split(" ")) - len(the_end.split(" "))
+epilogue_title = "EPILOGUE"
+
+remainder = 50000 - len(byline.split(" ")) - len(toc.split(" ")) - len(the_end.split(" ")) - len(epilogue_title.split(" "))
 
 def phrase_gen(wordcount):
 	"""Generates a string of given length (wordcount)."""
+
 	phrase = random.choice(words).strip()
+
 	for i in range(1, wordcount):
 		phrase = phrase + " " + random.choice(words).strip()
+
 	return phrase
+
+def para_gen():
+	"""Generates a paragraph of sentences of random length with random punctuation."""
+
+	punct = ['!', '?', ';', ',', ',', ',', ',', '.', '.', '.', '.', '.', '.', '.', '.']
+	punc = '.'
+	prev_punc = '.'
+
+	paragraph = []	
+	for i in range(5, random.randint(6, 30)):
+
+		if prev_punc in ['!', '?', '.']:
+			paragraph.append("%s%s " % (phrase_gen(random.randint(3, 15)).capitalize(), punc))
+		else:
+			paragraph.append("%s%s " % (phrase_gen(random.randint(3, 15)), punc))
+		prev_punc = punc
+		punc = random.choice(punct).strip()
+	punc = '.'
+	paragraph.append("%s%s\n\n" % (phrase_gen(random.randint(3, 15)).capitalize(), punc))
+	
+	return paragraph
+
+def epilogue_gen(wordcount):
+	"""Generates an epilogue of a given lenth, with just regular sentences."""
+
+	sentence_length = []
+	while wordcount >= 7:
+		n = random.randint(3, 13)
+		sentence_length.append(n)
+		wordcount = wordcount - n
+	sentence_length.append(wordcount)
+
+	epilogue = []
+
+	for i in sentence_length:
+		epilogue.append("%s" % phrase_gen(i).capitalize())
+
+	return epilogue
 
 if exists(novel):
 	print "'%s' exists, shall we wipe it? (Y,n)" % novel
 	quit = raw_input("> ").lower()
 	acquiesce = ['y', 'yes'] 
+
 	if quit in acquiesce or not quit:
 		open(novel, 'w').truncate()
 	else:
 		print "You appear to be experiencing writer's block. Goodbye."
-		exit(1)
+		exit(0)
 
 words = open("/usr/share/dict/words", 'r').readlines()
 output = open(novel, 'w')
@@ -57,19 +101,23 @@ for n, i in enumerate(table_contents):
 	output.write("\t%2d%s %s  \n" % (n + 1, "." * 10, i))
 	remainder = remainder - len(i.split(" "))
 
-output.write("\n__%s__ " % random.choice(words).upper().strip())
+while remainder >= 500:
+	paragraph = para_gen()
+	for i in paragraph:
+		output.write(i),
+		remainder = remainder - len(i.strip().split(" "))
 
-remainder = remainder - 2
+output.write("\n  \n#####%s  \n\n" % the_end)
 
-for i in range(remainder):
-	output.write("%s " % random.choice(words).strip())
+output.write("%s:  \n\n" % epilogue_title)
 
-output.write("%s. " % random.choice(words).strip())
-output.write("\n  \n#####%s  " % the_end)
+epilogue = epilogue_gen(remainder)
+for i in epilogue:
+	output.write("%s. " % i)
 
 print "Sending to publisher..."
 
 output.close()
 
 print "Congratulations! Your new masterpiece, %s,\nhas been listed atop the New York Times best sellers list." % book_title
-
+exit(0)
